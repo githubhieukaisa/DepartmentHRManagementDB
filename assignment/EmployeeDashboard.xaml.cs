82bbf2ca3f74f144888630735341573259f775ad
githubhieukaisa <vuahoarong@gmail.com>
2025-07-21 13:22:31 +0700

    add usecontrol for employee

--- assignment/EmployeeDashboard.xaml.cs
+++ assignment/EmployeeDashboard.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using assignment.EmployeeUseControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,35 @@ namespace assignment
         public EmployeeDashboard()
         {
             InitializeComponent();
+            LoadInitialContent();
+        }
+
+        private void LoadInitialContent()
+        {
+            tabControl.SelectedItem = tabControl.Items[0];
+            MainContent.Content = new EmployeeProfile();
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(tabControl.SelectedItem is TabItem selectedTab)
+            {
+                switch (selectedTab.Header.ToString())
+                {
+                    case "Profile":
+                        MainContent.Content = new EmployeeProfile();
+                        break;
+                    case "Project":
+                        MainContent.Content = new EmployeeProject();
+                        break;
+                    case "Task":
+                        MainContent.Content = new EmployeeTask();
+                        break;
+                    case "Team":
+                        MainContent.Content = new EmployeeTeam();
+                        break;
+                }
+            }
         }
     }
 }
---
+++ assignment/EmployeeUseControl/EmployeeTeam.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace assignment.EmployeeUseControl
+{
+    /// <summary>
+    /// Interaction logic for EmployeeTeam.xaml
+    /// </summary>
+    public partial class EmployeeTeam : UserControl
+    {
+        public EmployeeTeam()
+        {
+            InitializeComponent();
+        }
+    }
+}
